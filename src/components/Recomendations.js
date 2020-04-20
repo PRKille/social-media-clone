17f8afa7a4f45260dd31488c51af1bb3495c549b
Benjamin Thom <jaminthom@gmail.com>
2020-04-20 11:54:02 -0700

    update src/components/Recomendations.js to alllow passing props funtionality to ContactLinks.js component
    Co-authored-by: K. Wicz <katy@solovewicz>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/components/Recomendations.js
+++ src/components/Recomendations.js
@@ -1,1 +1,39 @@
 import React from "react";
+import ContactLink from "./ContactLink";
+
+const masterRecommendations = [
+  {
+    img: 'yoface.png',
+    name: 'Patty RICK',
+    link: '#'
+  },
+  {
+    img: 'julieandrewsfangirl.png',
+    name: 'Katy Lady',
+    link: '#'
+  },
+  {
+    img: 'stingbean.png',
+    name: 'We Jamin',
+    link: '#'
+  }
+];
+
+function Recommendations(){
+  return (
+    <React.Fragment>
+      <h2>Recommended Friends</h2>
+      <hr/>
+      {masterRecommendations.map((contactLink, index) => 
+        <ContactLink
+        img={contactLink.img}
+        name={contactLink.name}
+        link={contactLink.link}
+        key={index}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default Recommendations;
+
