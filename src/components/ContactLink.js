f6a2e805f27f4756f5554f3defac50f6b8edbd85
Benjamin Thom <jaminthom@gmail.com>
2020-04-20 11:51:44 -0700

    update src/components/ContactLink.js to include component elements and array
    Co-authored-by: K. Wicz <katy@solovewicz>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>

--- src/components/ContactLink.js
+++ src/components/ContactLink.js
@@ -1,1 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function ContactLink(props){
+  return (
+    <React.Fragment>
+      <img src={props.img} alt="Add this person to your friends"></img>
+      <p>{props.name}</p>
+      <button href={props.link}>Button</button>
+    </React.Fragment>
+  );
+}
+
+ContactLink.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  link: PropTypes.string
+}
+
+export default ContactLink;
